4e4fa7af92f9f2edd81a499c56c596ba0f49cb72
C J Silverio <ceejceej@gmail.com>
2015-03-15 20:54:52 -0700

    That was the weirdest thing ever:
    how did Atom get CR lineendings?

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-var    assert   = require('assert'),    bindings = require('bindings'),    jumpsuit = bindings('jumpsuit').jumphash	;module.exports = function wrapper(key, buckets){    assert(typeof key == 'number' && (key >= 0), 'you must pass a non-negative integer key');    assert(typeof buckets == 'number' && (buckets > 0), 'you must pass a positive integer bucketCount');    return jumpsuit(key, buckets);};
+var
+    assert   = require('assert'),
+    bindings = require('bindings'),
+    jumpsuit = bindings('jumpsuit').jumphash
+    ;
+
+module.exports = function wrapper(key, buckets)
+{
+    assert(typeof key == 'number' && (key >= 0), 'you must pass a non-negative integer key');
+    assert(typeof buckets == 'number' && (buckets > 0), 'you must pass a positive integer bucketCount');
+
+    return jumpsuit(key, buckets);
+};
